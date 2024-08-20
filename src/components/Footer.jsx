b6cc79c22beb0ca9d9fc8ffdabac04fd54faedab
faisal <faisalmunadhilramzy.com>
2024-08-20 14:50:15 +0700

    selesai

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div id="copyright">
     <div className="wrapper">
-        &copy; 2020. <b>RumahRafif.</b> All Rights Reserved.
+        &copy; 2020. <b>Faisal</b> All Rights Reserved.
     </div>
 </div>
   )
